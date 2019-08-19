96838057ded683c5cb81057d65a15b9d228a1f23
Albert Cheng <lptalbert@gmail.com>
2019-08-19 15:33:05 -0700

    Complete easy part of the queries

--- AA Questins/questions.rb
+++ AA Questins/questions.rb
@@ -45,13 +45,25 @@ class User
     SQL
     return nil unless user.length > 0
 
-    User.new(user.first) 
+    users = []
+    user.each do |u|
+      users << User.new(u)
+    end
+    users
   end
 
   def initialize(options)
     @id = options['id']
     @fname = options['fname']
     @lname = options['lname']
+  end
+
+  def authored_questions
+    Question.find_by_author_id(self.id)
+  end
+
+  def authored_replies
+    Reply.find_by_user_id(self.id)
   end
 end
 
@@ -77,11 +89,37 @@ class Question
     Question.new(question.first) 
   end
 
+  def self.find_by_author_id(author_id)
+    question = QuestionsDB.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        qauthor_id = ?;
+    SQL
+    return nil unless question.length > 0
+
+    questions = []
+    question.each do |q|
+      questions << Question.new(q)
+    end
+    questions 
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
     @qbody = options['qbody']
     @qauthor_id = options['qauthor_id']
+  end
+
+  def author
+    User.find_by_id(self.qauthor_id)
+  end
+
+  def replies
+    Reply.find_by_question_id(self.id)
   end
 end
 
@@ -162,7 +200,43 @@ class Reply
         id = ?;
     SQL
     return nil unless reply.length > 0
-    Reply.new(reply.first) 
+    Reply.new(reply.first)
+  end
+
+  def self.find_by_user_id(user_id)
+    reply = QuestionsDB.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        rauthor_id = ?;
+    SQL
+    return nil unless reply.length > 0
+
+    replies = []
+    reply.each do |r|
+      replies << Reply.new(r)
+    end
+    replies 
+  end
+
+  def self.find_by_question_id(question_id)
+    reply = QuestionsDB.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        subject_q = ?;
+    SQL
+    return nil unless reply.length > 0
+
+    replies = []
+    reply.each do |r|
+      replies << Reply.new(r)
+    end
+    replies 
   end
 
   def initialize(options)
@@ -171,5 +245,35 @@ class Reply
     @parent_id = options['parent_id']
     @rauthor_id = options['rauthor_id']
     @rbody = options['rbody']
+  end
+
+  def author
+    User.find_by_id(self.rauthor_id)
+  end
+
+  def question
+    Question.find_by_id(self.subject_q)
+  end
+
+  def parent_reply
+    Reply.find_by_id(self.parent_id)
+  end
+
+  def child_replies
+    reply = QuestionsDB.instance.execute(<<-SQL, self.id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        parent_id = ?;
+    SQL
+    return nil unless reply.length > 0
+
+    replies = []
+    reply.each do |r|
+      replies << Reply.new(r)
+    end
+    replies 
   end
 end
